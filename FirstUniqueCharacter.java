96b0ff885c61be87904c70e2d9eb5005a58d8b56
rupesh-singh <rupesh08singh08@gmail.com>
2020-05-05 12:53:43 +0530

    Firt Unique Character solution

---
+++ FirstUniqueCharacter.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] freq=new int[26];
+        
+        for(char c: s.toCharArray()){
+            freq[c-'a']++;
+        }
+        
+        for(char c: s.toCharArray()){
+            if(freq[c-'a']==1)
+                return s.indexOf(c);
+        }
+        return -1;
+    }
+}
