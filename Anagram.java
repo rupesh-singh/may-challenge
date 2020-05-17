5089ba1f575dfd29f08450cceff2afd4468c20cf
rupesh-singh <rupesh08singh08@gmail.com>
2020-05-17 19:34:54 +0530

    Day 17 Solution

---
+++ Anagram.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        
+        List<Integer> li = new ArrayList<>();
+        int[] m= new int[26];
+        
+        for(int i=0;i<p.length();i++){
+            m[p.charAt(i)-'a']++;
+        }
+        
+        int start=0,end=0;
+        while(end<s.length()){
+            
+            if(m[s.charAt(end)-'a']>0){
+                m[s.charAt(end++)-'a']--;
+            
+                    if(end-start == p.length())
+                        li.add(start);
+            }
+            
+            else if(start==end)
+            {
+                start++;
+                end++;
+            }
+            
+            else{
+                m[s.charAt(start++)-'a']++;
+            }
+            
+            
+        }
+    
+    
+        return li;
+    }
+    
+}
